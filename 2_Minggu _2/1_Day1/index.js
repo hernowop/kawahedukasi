482bf9aed99c14157f93da2d9c67ccf5a8c4660c
hernowo.programmer <hernowo.programmer@gmail.com>
2023-10-10 15:42:45 +0700

    Day 1 Week2

---
+++ 2_Minggu _2/1_Day1/index.js
@@ -0,0 +1,35 @@
+// let umur = 8;
+// let tipePekerjaan = 3;
+// let jawaban;
+
+// if(umur > 7 && tipePekerjaan > 2){
+//     jawaban = "Masuk";
+// }
+
+// if(!undefined){
+//     console.log(jawaban);
+// }
+
+// //TRULY && FALSY
+
+// //Boolean
+// let bool1 = true //TRULY
+// let bool2 = false //FALSY
+
+// //Number
+// let numb1 = 0; // FALSY => NaN
+// let numb2 = 9; // TRULY => Infinity
+// let numb3 = -9 // TRULY 
+// let numb3 = 7.7 // TRULY
+
+// //String
+// let str1 = `asdsadasd` // TRULY
+// let str2 = ``; //FALSY
+
+// // undefined, null => FALSY;
+
+// // Converting Value => !
+
+// if(str2){
+//     console.log("Code Akan Berjalan");
+// }
