743b13ff9160a6bfd797941a29a6f4ce42b09975
hernowo.programmer <hernowo.programmer@gmail.com>
2023-10-11 09:17:27 +0700

    Materi Week 2 Day 2

---
+++ 2_Minggu _2/2_Day2/switch.js
@@ -0,0 +1,50 @@
+//TODO: SWITCH CASE
+//Special Condition
+
+//SYNTAX SWITCH CASE
+/**
+ * switch(case){
+ *  case [true case];
+ *      action
+ *      break
+ *  case [true case];
+ *      action
+ *      break
+ *  default (optional)
+ *      action
+ *      break
+ * }
+ */
+
+// let str = `Danu`; 
+
+// //In if
+// if(str == "Danu"){
+//     console.log(`yak benar`);
+// }else{
+//     console.log(`yak salah`);
+// }
+
+
+//In Switch
+//let str = `Danu`
+// switch(str){
+//     case `Danu`:
+//         console.log(`yak benar 1`);
+//         break;
+//     case `hh`:
+//         console.log(`yak benar 2`);
+//         break;
+//     case `gg`:
+//         console.log(`yak benar 3`);
+//         break;
+//     default:
+//         console.log(`yak salah`);
+//         break;
+// }
+
+let a = 2;
+switch(a){
+    case (a >= 5 ? console.log(`Benar 10`) : console.log(`Salah 40`)):
+    break;
+}
---
+++ 2_Minggu _2/2_Day2/tennary.js
@@ -0,0 +1,45 @@
+//TODO: TERNARY
+/**
+ * ? SYNTAX IF
+ * if(true condition){
+ *      action if true;
+ * }else{
+ *      action if false;
+ * }
+ * 
+ * ? SYANTAX TERNARY
+ * true condition ? action if true : action if false
+ */
+
+// // USE IF ELSE
+// if( 1 > 2 ){
+//     console.log("benar");
+// } else {
+//     console.log("salah");
+// }
+
+// // BASIC TERNARY
+// 3 > 2 ? console.log(`benar`) : console.log(`salah`);
+
+// NESTED TERNARY
+// 1  > 2
+// ? console.log(`true 1`)
+// : ( 2 > 3
+//     ? console.log(`true 2`)
+//     : console.log(`false`)
+// );
+
+//KINDA TERNARY
+// let str = `Danu Itsnan Habibi`;
+// str.length > 10 ? console.log(`Terlalu Panjang`) : console.log(`OK`);
+
+//VARIABLE WITH TERNARY
+// let str = `Danu `;
+// const answer = str.length > 10 ? 'Terlalu Panjang' : `OK`;
+// console.log(answer);
+
+//CONSOLE WITH TERNARY
+// let str = `Danu Istnan Habibi`;
+// console.log(str.length > 10 ? `Terlalu Panjang` : `OK`);
+
+3 > 2 ? console.log(`Benar`) : null;
